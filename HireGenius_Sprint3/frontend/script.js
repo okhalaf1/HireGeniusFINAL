2e2f9ef38f186c4c2e30aa15d6e0349c6c3a0808
Omar Khalaf <okhalaf1@live.maryville.edu>
2025-05-05 03:54:25 +0800

    Initial commit of HireGenius project

--- HireGenius_Sprint3/frontend/script.js
+++ HireGenius_Sprint3/frontend/script.js
@@ -1,1 +1,20 @@
-console.log('HireGenius frontend loaded.');
+function showLogin() {
+    document.getElementById("output").innerText = "Login form goes here...";
+  }
+  
+  function showSignup() {
+    document.getElementById("output").innerText = "Signup form goes here...";
+  }
+  
+  function loadJobs() {
+    fetch("http://localhost:5000/jobs")
+      .then(response => response.json())
+      .then(data => {
+        document.getElementById("output").innerText = JSON.stringify(data, null, 2);
+      })
+      .catch(err => {
+        document.getElementById("output").innerText = "Could not load jobs.";
+        console.error(err);
+      });
+  }
+  
